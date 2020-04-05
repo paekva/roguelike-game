5e106362df246d80413067310e466dbfd0ff1dcf
kate <paekva@yandex.ru>
2020-04-05 11:38:26 +0300

    added progress bar redrawing on health and energy change

--- src/client/sideBar.js
+++ src/client/sideBar.js
@@ -1,40 +1,74 @@
 const sideBarSize = tile_size * 5;
-const progressBarAreaHeight = sideBarSize/3;
+const progressBarAreaHeight = sideBarSize / 2.8;
 const progressBarMargin = tile_size;
 const progressBarAreaPadding = progressBarAreaHeight / 8;
 
 const barSize = {
-	w: tile_size/3,
-	h: tile_size
+	w: tile_size / 3,
+	h: tile_size,
 };
 const borderRadius = 20;
+
+const coordinatesE = {
+	x: progressBarAreaPadding + progressBarMargin,
+	y: progressBarAreaPadding,
+};
+const coordinatesH = {
+	x: progressBarAreaPadding + progressBarMargin * 3,
+	y: progressBarAreaPadding,
+};
 
 const drawProgressBar = (title, percentage, color, coordinates) => {
 	side_bar.stroke(0);
 
 	side_bar.fill(255);
-	side_bar.rect(coordinates.x, coordinates.y, barSize.w, barSize.h, borderRadius);
+	side_bar.rect(
+		coordinates.x,
+		coordinates.y,
+		barSize.w,
+		barSize.h,
+		borderRadius
+	);
 
 	side_bar.fill(color);
-	side_bar.rect(coordinates.x, coordinates.y + barSize.h * (1 - percentage), barSize.w, barSize.h * percentage, percentage < 0.9 ? 0 : borderRadius, percentage < 0.9 ? 0 : borderRadius, borderRadius, borderRadius);
-
-	side_bar.noFill();
+	side_bar.rect(
+		coordinates.x,
+		coordinates.y + barSize.h * (1 - percentage),
+		barSize.w,
+		barSize.h * percentage,
+		percentage < 0.9 ? 0 : borderRadius,
+		percentage < 0.9 ? 0 : borderRadius,
+		borderRadius,
+		borderRadius
+	);
 	side_bar.noStroke();
 
-
+	side_bar.fill(0);
+	side_bar.textSize(14);
+	side_bar.text(
+		title,
+		coordinates.x,
+		progressBarAreaHeight - progressBarAreaPadding
+	);
+	side_bar.noFill();
 };
 
 const drawSideBar = () => {
-	const coordinatesE = {
-		x: progressBarAreaPadding + progressBarMargin,
-		y: progressBarAreaPadding
-	};
-	const coordinatesH = {
-		x: progressBarAreaPadding + progressBarMargin * 3,
-		y: progressBarAreaPadding
-	};
-	drawProgressBar('Energy', 0.95, 'blue', coordinatesE);
-	drawProgressBar('Health', 0.7, 'red', coordinatesH);
+	if (controller && controller.player_human) {
+		const human = controller.player_human.hero;
+		drawProgressBar(
+			'Energy',
+			human.energy / human.max_energy,
+			'blue',
+			coordinatesE
+		);
+		drawProgressBar(
+			'Health',
+			human.health / human.max_health,
+			'red',
+			coordinatesH
+		);
+	}
 };
 
 const initSideBar = () => {
@@ -49,9 +83,11 @@ const initSideBar = () => {
 };
 
 const initProgressBarsArea = () => {
-	const progressCanvas = function( sketch ) {
+	const progressCanvas = function(sketch) {
 		sketch.setup = function() {
-			sketch.createCanvas(sideBarSize, progressBarAreaHeight).parent("progressBar").c;
+			sketch
+				.createCanvas(sideBarSize, progressBarAreaHeight)
+				.parent('progressBar').c;
 			side_bar = sketch;
 		};
 	};
