622ca86b8c6097226961fa6f58203da2bc6fa9ce
paekva <paekva@yandex.ru>
2020-04-16 13:07:24 +0300

    added new panel for start menu

--- src/client/settingsMenu.js
+++ src/client/settingsMenu.js
@@ -1,51 +1,61 @@
 const initSettingMenu = () => {
-	document.getElementById('startBtn').addEventListener('click', onStartGame);
+  document.getElementById("nextBtn").addEventListener("click", onNextClick);
+  document.getElementById("startBtn").addEventListener("click", onStartGame);
 
-	const listwrapper = document.getElementById('charactersList');
-	characterIconsLinks.forEach(imageLink => {
-		const img = document.createElement('img');
-		img.src = imageLink;
-		img.width = 100;
-		img.height = 100;
-		const holder = document.createElement('div');
-		holder.className = 'holder';
-		holder.dataset.link = imageLink;
+  const listwrapper = document.getElementById("charactersList");
+  characterIconsLinks.forEach(imageLink => {
+    const img = document.createElement("img");
+    img.src = imageLink;
+    const size = window.innerHeight / 13;
+    img.height = size;
+    img.width = size;
 
-		holder.addEventListener('click', event => {
-			heroIconLink = event.currentTarget.dataset.link;
-			drawIcons();
-		});
+    const holder = document.createElement("div");
+    holder.className = "holder";
+    holder.dataset.link = imageLink;
 
-		holder.appendChild(img);
-		listwrapper.appendChild(holder);
-	});
+    holder.addEventListener("click", event => {
+      heroIconLink = event.currentTarget.dataset.link;
+      drawIcons();
+    });
+
+    holder.appendChild(img);
+    listwrapper.appendChild(holder);
+  });
 };
 
 const onStartGame = () => {
-	document.getElementById('startScreen').style.display = 'none';
+  document.getElementById("startScreen").style.display = "none";
 
-	characterIconsLinks.forEach(link => {
-		if (link !== heroIconLink)
-			monsterIcons.push(battlefield_map.loadImage(link));
-	});
+  characterIconsLinks.forEach(link => {
+    if (link !== heroIconLink)
+      monsterIcons.push(battlefield_map.loadImage(link));
+  });
 
-	heroIcon = battlefield_map.loadImage(heroIconLink);
+  heroIcon = battlefield_map.loadImage(heroIconLink);
 
-	socket.emit(SocketEmitEventType.CHARACTER_SELECTED, 'data');
+  socket.emit(SocketEmitEventType.CHARACTER_SELECTED, "data");
 };
 
 const drawIcons = () => {
-	const items = document.getElementById('charactersList').childNodes;
-	items.forEach(item => {
-		item.className =
-			item.dataset && item.dataset.link === heroIconLink
-				? 'selected'
-				: 'holder';
-	});
+  const items = document.getElementById("charactersList").childNodes;
+  items.forEach(item => {
+    item.className =
+      item.dataset && item.dataset.link === heroIconLink
+        ? "selected"
+        : "holder";
+  });
 
-	const startBtn = document.getElementById('startBtn');
-	if (startBtn.disabled) {
-		startBtn.className = 'startBtn';
-		startBtn.disabled = false;
-	}
+  const nextBtn = document.getElementById("nextBtn");
+  if (nextBtn.disabled) {
+    nextBtn.className = "startBtn";
+    nextBtn.disabled = false;
+  }
+};
+
+const onNextClick = () => {
+  (document.getElementById("nextBtn").style.display = "none");
+  (document.getElementById("characters").style.display = "none");
+  (document.getElementById("startBtn").style.display = "block");
+  (document.getElementById("another").style.display = "block");
 };
