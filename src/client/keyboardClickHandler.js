c28b6ae9a4625feb1b1eb73dd9b911160d61883b
paekva <paekva@yandex.ru>
2020-04-18 17:42:03 +0300

    tryout of sounding

--- src/client/keyboardClickHandler.js
+++ src/client/keyboardClickHandler.js
@@ -1,62 +1,83 @@
 function keyPressed() {
-	switch (keyCode) {
-		case 39: {
-			if (!isDefaultKeyboard)
-				socket.emit(SocketEmitEventType.MOVE_HERO_TILE, 'right');
-			break;
-		}
-		case 37: {
-			if (!isDefaultKeyboard)
-				socket.emit(SocketEmitEventType.MOVE_HERO_TILE, 'left');
-			break;
-		}
-		case 38: {
-			if (!isDefaultKeyboard)
-				socket.emit(SocketEmitEventType.MOVE_HERO_TILE, 'up');
-			break;
-		}
-		case 40: {
-			if (!isDefaultKeyboard)
-				socket.emit(SocketEmitEventType.MOVE_HERO_TILE, 'down');
-			break;
-		}
-		case 27: {
-			document.getElementById('startScreen').style.display = 'flex';
-			document.getElementById('startBtn').innerText = 'Continue';
-			document.getElementById('startBtn').addEventListener('onclick', () => {
-				document.getElementById('startScreen').style.display = 'none';
-			});
-			break;
-		}
-		case 68: {
-			if (isDefaultKeyboard)
-				socket.emit(SocketEmitEventType.MOVE_HERO_TILE, 'right');
-			break;
-		}
-		case 65: {
-			if (isDefaultKeyboard)
-				socket.emit(SocketEmitEventType.MOVE_HERO_TILE, 'left');
-			break;
-		}
-		case 87: {
-			if (isDefaultKeyboard)
-				socket.emit(SocketEmitEventType.MOVE_HERO_TILE, 'up');
-			break;
-		}
-		case 83: {
-			if (isDefaultKeyboard)
-				socket.emit(SocketEmitEventType.MOVE_HERO_TILE, 'down');
-			break;
-		}
-		case 59: {
-			if (battlefield_active) {
-				console.log('End turn of player: ' + controller.turn);
-				socket.emit(SocketEmitEventType.END_TURN, { index: controller.turn });
-			}
-			break;
-		}
-		case 32: {
-			socket.emit(SocketEmitEventType.SKIP_TURN);
-		}
-	}
+  switch (keyCode) {
+    case 39: {
+      if (!isDefaultKeyboard) {
+        playStepSound();
+        socket.emit(SocketEmitEventType.MOVE_HERO_TILE, "right");
+      }
+      break;
+    }
+    case 37: {
+      if (!isDefaultKeyboard) {
+        playStepSound();
+        socket.emit(SocketEmitEventType.MOVE_HERO_TILE, "left");
+      }
+      break;
+    }
+    case 38: {
+      if (!isDefaultKeyboard) {
+        playStepSound();
+        socket.emit(SocketEmitEventType.MOVE_HERO_TILE, "up");
+      }
+      break;
+    }
+    case 40: {
+      if (!isDefaultKeyboard) {
+        playStepSound();
+        socket.emit(SocketEmitEventType.MOVE_HERO_TILE, "down");
+      }
+      break;
+    }
+    case 27: {
+      document.getElementById("startScreen").style.display = "flex";
+      document.getElementById("startBtn").innerText = "Continue";
+      document.getElementById("startBtn").addEventListener("onclick", () => {
+        document.getElementById("startScreen").style.display = "none";
+      });
+      break;
+    }
+    case 68: {
+      if (isDefaultKeyboard) {
+        playStepSound();
+        socket.emit(SocketEmitEventType.MOVE_HERO_TILE, "right");
+      }
+      break;
+    }
+    case 65: {
+      if (isDefaultKeyboard) {
+        playStepSound();
+        socket.emit(SocketEmitEventType.MOVE_HERO_TILE, "left");
+      }
+      break;
+    }
+    case 87: {
+      if (isDefaultKeyboard) {
+        playStepSound();
+        socket.emit(SocketEmitEventType.MOVE_HERO_TILE, "up");
+      }
+      break;
+    }
+    case 83: {
+      if (isDefaultKeyboard) {
+        playStepSound();
+        socket.emit(SocketEmitEventType.MOVE_HERO_TILE, "down");
+      }
+      break;
+    }
+    case 59: {
+      if (battlefield_active) {
+        console.log("End turn of player: " + controller.turn);
+        socket.emit(SocketEmitEventType.END_TURN, { index: controller.turn });
+      }
+      break;
+    }
+    case 32: {
+      socket.emit(SocketEmitEventType.SKIP_TURN);
+    }
+  }
 }
+
+const audio = new Audio("/public/audio/punch-sound-effect.mp3");
+const playStepSound = () => {
+  if (isSoundOn) audio.play();
+};
--- src/client/settingsHandlers.js
+++ src/client/settingsHandlers.js
@@ -1,77 +1,77 @@
 const initAllSettingsHandlers = () => {
-	document
-		.getElementsByName('scaleOptions')
-		.forEach(el => el.addEventListener('click', onScaleOptionChange));
+  document
+    .getElementsByName("scaleOptions")
+    .forEach(el => el.addEventListener("click", onScaleOptionChange));
 
-	document
-		.getElementsByName('keyboardOptions')
-		.forEach(el => el.addEventListener('click', onKeyboardOptionChange));
+  document
+    .getElementsByName("keyboardOptions")
+    .forEach(el => el.addEventListener("click", onKeyboardOptionChange));
 
-	document
-		.getElementsByName('musicOptions')
-		.forEach(el => el.addEventListener('click', onMusicOptionChange));
+  document
+    .getElementsByName("musicOptions")
+    .forEach(el => el.addEventListener("click", onMusicOptionChange));
 
-	document
-		.getElementsByName('soundOptions')
-		.forEach(el => el.addEventListener('click', onSoundOptionChange));
+  document
+    .getElementsByName("soundOptions")
+    .forEach(el => el.addEventListener("click", onSoundOptionChange));
 };
 
 const onScaleOptionChange = event => {
-	const optionType = event.target.id;
-	switch (optionType) {
-		case 'sc10': {
-			tile_size = tile_default_size * 1.1;
-			break;
-		}
-		case 'sc50': {
-			tile_size = tile_default_size * 1.5;
-			break;
-		}
-		case 'sc80': {
-			tile_size = tile_default_size * 1.8;
-			break;
-		}
-		default:
-			tile_size = tile_default_size;
-			break;
-	}
+  const optionType = event.target.id;
+  switch (optionType) {
+    case "sc10": {
+      tile_size = tile_default_size * 1.1;
+      break;
+    }
+    case "sc50": {
+      tile_size = tile_default_size * 1.5;
+      break;
+    }
+    case "sc80": {
+      tile_size = tile_default_size * 1.8;
+      break;
+    }
+    default:
+      tile_size = tile_default_size;
+      break;
+  }
 };
 
 const onKeyboardOptionChange = event => {
-	const optionType = event.target.id;
-	switch (optionType) {
-		case 'arrows': {
-			isDefaultKeyboard = false;
-			return;
-		}
-		default:
-			isDefaultKeyboard = true;
-			return;
-	}
+  const optionType = event.target.id;
+  switch (optionType) {
+    case "arrows": {
+      isDefaultKeyboard = false;
+      return;
+    }
+    default:
+      isDefaultKeyboard = true;
+      return;
+  }
 };
 
 const onMusicOptionChange = event => {
-	const optionType = event.target.id;
-	switch (optionType) {
-		case 'musicOn': {
-			isMusicOn = false;
-			return;
-		}
-		default:
-			isMusicOn = true;
-			return;
-	}
+  const optionType = event.target.id;
+  switch (optionType) {
+    case "musicOn": {
+      isMusicOn = false;
+      return;
+    }
+    default:
+      isMusicOn = true;
+      return;
+  }
 };
 
 const onSoundOptionChange = event => {
-	const optionType = event.target.id;
-	switch (optionType) {
-		case 'soundOn': {
-			isSoundOn = false;
-			return;
-		}
-		default:
-			isSoundOn = true;
-			return;
-	}
+  const optionType = event.target.id;
+  switch (optionType) {
+    case "soundOn": {
+      isSoundOn = true;
+      return;
+    }
+    default:
+      isSoundOn = false;
+      return;
+  }
 };
