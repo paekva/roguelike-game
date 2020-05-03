2641a5dd5272ce52397eca130afd417fe24c4e77
paekva <paekva@yandex.ru>
2020-05-03 18:40:56 +0300

    modifications file refactoring

--- src/client/modifications.js
+++ src/client/modifications.js
@@ -1,13 +1,6 @@
 const onOpenModificationScreenEvent = () => {
   document.getElementById("modificationsScreen").style.display = "flex";
-
-  document
-    .getElementById("backBtn")
-    .addEventListener("click", closeModificationsWindow);
-
-  document.getElementById("saveBtn").addEventListener("click", () => {
-    socket.emit(SocketEmitEventType.CHANGE_MODIFICATIONS, currentModifications);
-  });
+  addEventListenersForModificationsScreen();
 
   const holder = document.getElementById("modificationsHolder");
   if (holder.childNodes.length === 0) addModifications();
@@ -18,8 +11,20 @@ const closeModificationsWindow = () => {
   document.getElementById("overlayLayer").style.display = "none";
 };
 
+const addEventListenersForModificationsScreen = () => {
+  document
+    .getElementById("backBtn")
+    .addEventListener("click", closeModificationsWindow);
+
+  document.getElementById("saveBtn").addEventListener("click", () => {
+    socket.emit(SocketEmitEventType.CHANGE_MODIFICATIONS, currentModifications);
+  });
+};
+
 const checkIfModificationsScreenOpened = () => {
-    return document.getElementById("modificationsScreen").style.display === "flex";
+  return (
+    document.getElementById("modificationsScreen").style.display === "flex"
+  );
 };
 
 const modificationParams = [
@@ -34,34 +39,51 @@ const addModifications = () => {
   const holder = document.getElementById("modificationsHolder");
   controller.hero.modifications &&
     controller.hero.modifications.forEach(modification => {
-      const el = document.createElement("div");
-      el.className = "modificationOption";
+      const newElement = createItemForSingleModification();
+      const childList = newElement.children;
 
-      let clone = document.importNode(
-        document.getElementById("modificationOptions").content,
-        true
-      );
-      el.appendChild(clone);
-      const childList = el.children;
-      childList[0].innerHTML = modification.name;
+      applyModificationName(childList[0], modification);
+      applyModificationCharacteristics(childList[1], modification);
+      applyModificationStatus(childList[2], modification);
 
-      const modificationItems = document.createElement("div");
-      modificationItems.style.display = "flex";
-      modificationItems.style.flexDirection = "column";
-
-      modificationParams.forEach(param => {
-        const item = document.createElement("div");
-        item.innerHTML = `${param}:\t${modification[param]}`;
-        modificationItems.appendChild(item);
-      });
-      childList[1].appendChild(modificationItems);
-      childList[2].setAttribute("name", modification.name);
-      childList[2].addEventListener("mousedown", e => {
-        onModificationUpdate(modification.name);
-      });
-
-      holder.appendChild(el);
+      holder.appendChild(newElement);
     });
+};
+
+const createItemForSingleModification = () => {
+  const el = document.createElement("div");
+  el.className = "modificationOption";
+
+  let clone = document.importNode(
+    document.getElementById("modificationOptions").content,
+    true
+  );
+  el.appendChild(clone);
+  return el;
+};
+
+const applyModificationName = (child, modification) => {
+  child.innerHTML = modification.name;
+};
+
+const applyModificationCharacteristics = (child, modification) => {
+  const modificationItems = document.createElement("div");
+  modificationItems.style.display = "flex";
+  modificationItems.style.flexDirection = "column";
+
+  modificationParams.forEach(param => {
+    const item = document.createElement("div");
+    item.innerHTML = `${param}:\t${modification[param]}`;
+    modificationItems.appendChild(item);
+  });
+  child.appendChild(modificationItems);
+};
+
+const applyModificationStatus = (child, modification) => {
+  child.setAttribute("name", modification.name);
+  child.addEventListener("mousedown", () => {
+    onModificationUpdate(modification.name);
+  });
 };
 
 const currentModifications = [];
